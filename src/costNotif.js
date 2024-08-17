ebf6257f1f06919b851262c4373381ad43955440
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-08-18 06:35:45 +0900

    add costNotif

---
+++ src/costNotif.js
@@ -0,0 +1,37 @@
+// const serverless = require("serverless-http");
+// const express = require("express");
+// const app = express();
+
+const { IncomingWebhook } = require('@slack/webhook');
+const  AWS = require("aws-sdk");
+const dayjs = require('dayjs')
+
+// SlackのWebhook URLを環境変数から取得
+const webhookUrl = process.env.SLACK_WEBHOOK_URL;
+const webhook = new IncomingWebhook(webhookUrl);
+
+// AWS Cost Explorerの設定
+const date = dayjs();
+const startDate = date.format("YYYY-MM-01");
+const endDate = date.add(1, "months").format("YYYY-MM-01");
+const ce = new AWS.CostExplorer({region: 'us-east-1'});
+const params = {
+    TimePeriod: {
+        Start: startDate,
+        End: endDate
+    },
+    Granularity: 'MONTHLY',
+    Metrics: ['UnblendedCost']
+};
+
+// Lambdaハンドラー関数
+async function costNotif() {
+    const costAndUsage = await ce.getCostAndUsage(params).promise();
+    const useCost = costAndUsage.ResultsVyTime[0].Total.UnblendedCost.Amount;
+    await webhook.send({
+        text: `今月の使用料は: ${useCost} USDです`
+    });
+}
+
+// 関数をエクスポート
+module.exports = costNotif;
