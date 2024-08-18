10c02f9ca6405a9185a3b01c2f76a8c758f50458
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-08-19 05:54:54 +0900

    feat: AWS利用料のDayly通知完成

--- src/costNotif.js
+++ src/costNotif.js
@@ -1,5 +1,5 @@
 const { IncomingWebhook } = require('@slack/webhook');
-const  AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 const dayjs = require('dayjs')
 
 // SlackのWebhook URLを環境変数から取得
@@ -10,23 +10,23 @@ const webhook = new IncomingWebhook(webhookUrl);
 const date = dayjs();
 const startDate = date.format("YYYY-MM-01");
 const endDate = date.add(1, "months").format("YYYY-MM-01");
-const ce = new AWS.CostExplorer({region: 'us-east-1'});
+const ce = new AWS.CostExplorer({ region: 'us-east-1' });
 const params = {
-    TimePeriod: {
-        Start: startDate,
-        End: endDate
-    },
-    Granularity: 'MONTHLY',
-    Metrics: ['UnblendedCost']
+  TimePeriod: {
+    Start: startDate,
+    End: endDate
+  },
+  Granularity: 'MONTHLY',
+  Metrics: ['UnblendedCost']
 };
 
 // Lambdaハンドラー関数
 async function costNotif() {
-    const costAndUsage = await ce.getCostAndUsage(params).promise();
-    const useCost = costAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount;
-    await webhook.send({
-        text: `今月の使用料は: ${useCost} USDです`
-    });
+  const costAndUsage = await ce.getCostAndUsage(params).promise();
+  const useCost = costAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount;
+  await webhook.send({
+    text: `今月の使用料は: ${useCost} USDです`
+  });
 }
 
 // 関数をエクスポート
